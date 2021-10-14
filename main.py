038cb791225302aaa1774c6d790063295729b160
qdfeifei <xiaofei.wu1216@gmail.com>
2021-10-14 20:15:01 +0100

    import pandas and csv airplane and titanic

--- main.py
+++ main.py
@@ -4,4 +4,8 @@
 import pandas
 df=pandas.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv")
 print(df.describe())
-#ssssss
+print(df.head())
+
+df2=pandas.read_csv("titanic.csv")
+print(df2.describe())
+print(df2.head())
