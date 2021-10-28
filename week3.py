28d19f6eec367530940e39e0a9792b172e432cd6
qdfeifei <xiaofei.wu1216@gmail.com>
2021-10-28 20:06:31 +0100

    week3 main py pushed to Github

--- week3.py
+++ week3.py
@@ -3,7 +3,6 @@ import pandas
 df=pandas.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv")
 print(df.describe())
 print(df.head())
-
 df2=pandas.read_csv("titanic.csv")
 print(df2.describe())
 print(df2.head())
