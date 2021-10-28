05b63dd9f662ebdee6cb56bf04ba44680d4b1e4d
qdfeifei <xiaofei.wu1216@gmail.com>
2021-10-28 19:57:58 +0100

    week5 slicing info from API

---
+++ week3.py
@@ -0,0 +1,10 @@
+#main py pushed to Github for week3
+
+import pandas
+df=pandas.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv")
+print(df.describe())
+print(df.head())
+
+df2=pandas.read_csv("titanic.csv")
+print(df2.describe())
+print(df2.head())
