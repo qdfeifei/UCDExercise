eb25a67489dfe951260ec9d1a27e7efd0aadf46c
qdfeifei <xiaofei.wu1216@gmail.com>
2021-10-28 20:01:05 +0100

    week4 working with csv

--- week4.py
+++ week4.py
@@ -3,7 +3,6 @@ import pandas as pd
 netflix_data= pd.read_csv("netflix_titles.csv")
 print(netflix_data.head())
 print(netflix_data.shape)
-
 #pring missing value
 missing_values_count = netflix_data.isnull().sum()
 print(missing_values_count[0:12])
